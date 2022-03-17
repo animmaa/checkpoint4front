e12a4e872d754d7dbc072e94c7e19089bf4158ed
davy lebris <davylebris@gmail.com>
2022-03-17 16:06:06 +0100

    ajout page creation plat

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import ListDishes from './components/pages/listDishes/ListDishes';
 import SearchDishe from './components/pages/searchDishe/SearchDishe';
 import Dishe from './components/pages/dishe/Dishe';
 import './global.scss';
+import Login from './components/login/Login';
+import CreateDishes from './components/pages/createDishes/CreateDishes';
 
 const App = () => (
   <div className="App">
     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/create" element={<CreateDishes />} />
       <Route path="/listdishes" element={<ListDishes />} />
       <Route path="/listdishes/:id" element={<ListDishes />} />
       <Route path="/searchdishe" element={<SearchDishe />} />
